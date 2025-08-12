961ba83fd823d174236ca7658084a58d7ed31969
silentarrowz <silentarrowz@gmail.com>
2025-08-12 22:54:21 +0530

    history and addtohistory added

--- src/stores/useWeatherStore.ts
+++ src/stores/useWeatherStore.ts
@@ -3,8 +3,13 @@ import type { WeatherState } from '../types';
 
 
 export const useWeatherStore = create<WeatherState>((set) => ({  
+ history: [],
   currentWeather: null,
-  loading: false,  
+  loading: false,
+  addToHistory: (city) =>
+    set((state) => ({
+      history: [city, ...state.history.filter((c) => c.toLowerCase() !== city.toLowerCase())].slice(0, 5),
+    })),
   setCurrentWeather: (data) => set({ currentWeather: data }),
   setLoading: (v) => set({ loading: v }),
 }));
